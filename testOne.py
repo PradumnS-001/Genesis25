a7657044da1984722cf90a51b00eac14fe21ea4d
Pradumn Singh <pradumnsingh2200@gmail.com>
2025-03-14 00:07:18 +0530

    added notes and program One

---
+++ testOne.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+# Callback function for trackbars
+def nothing(x):
+    pass
+
+# Create a window for trackbars
+cv2.namedWindow("Trackbars")
+
+# Create trackbars to adjust HSV range
+cv2.createTrackbar("Lower H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("Lower S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("Lower V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("Upper H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("Upper S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("Upper V", "Trackbars", 255, 255, nothing)
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Convert frame to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Get trackbar values
+    lower_h = cv2.getTrackbarPos("Lower H", "Trackbars")
+    lower_s = cv2.getTrackbarPos("Lower S", "Trackbars")
+    lower_v = cv2.getTrackbarPos("Lower V", "Trackbars")
+    upper_h = cv2.getTrackbarPos("Upper H", "Trackbars")
+    upper_s = cv2.getTrackbarPos("Upper S", "Trackbars")
+    upper_v = cv2.getTrackbarPos("Upper V", "Trackbars")
+
+    lower_bound = np.array([lower_h, lower_s, lower_v])
+    upper_bound = np.array([upper_h, upper_s, upper_v])
+
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) > 1000: #Guess why we use this
+            cv2.drawContours(frame, contours, -1, (0, 255, 0), 2) # Draw contours on the frame
+
+    
+    cv2.imshow("Original with Contours", frame)
+    cv2.imshow("Mask", mask)
+
+    # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
